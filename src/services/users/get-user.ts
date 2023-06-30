6d675af3dd0cf7b7d92e1081286320ff5d581c48
teppei <teppei0830.4869@gmail.com>
2023-06-30 21:14:20 +0900

    API クライアントの設定をしました。

---
+++ src/services/users/get-user.ts
@@ -0,0 +1,23 @@
+import type { ApiContext, User } from "../types/data";
+import { fetcher } from "../../util";
+
+export type GetUserParams = {
+  id: number;
+};
+
+const getUser = async (
+  context: ApiContext,
+  { id }: GetUserParams
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, "")}/users/${id}`,
+    {
+      headers: {
+        Accept: "application/json",
+        "Context-Type": "application/json",
+      },
+    }
+  );
+};
+
+export default getUser;
