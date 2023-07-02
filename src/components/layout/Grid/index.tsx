bc908bd8f7feb971f70ce6197114b91b7faeec32
teppei <teppei0830.4869@gmail.com>
2023-07-02 22:02:43 +0900

    新たに src/components/layout/Grid/index.tsx を作成しました。
    Box コンポーネントを継承した Grid コンポーネントを定義しています。

---
+++ src/components/layout/Grid/index.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import Box, { BoxProps } from "@/components/layout/Box/index";
+import type {
+  Responsive,
+  CSSPropertyGridArea,
+  CSSPropertyGridAutoFlow,
+  CSSPropertyGridColumn,
+  CSSPropertyGridRow,
+} from "@/types/style";
+import { toPropValue } from "@/util/styles";
+
+type GridProps = BoxProps & {
+  gridGap?: Responsive<string>;
+  gridColumnGap?: Responsive<string>;
+  gridRowGap?: Responsive<string>;
+  gridColumn?: Responsive<CSSPropertyGridColumn>;
+  gridRow?: Responsive<CSSPropertyGridRow>;
+  gridAutoFlow?: Responsive<CSSPropertyGridAutoFlow>;
+  gridAutoColumns?: Responsive<string>;
+  gridAutoRows?: Responsive<string>;
+  gridTemplateColumns?: Responsive<string>;
+  gridTemplateRows?: Responsive<string>;
+  gridTemplateAreas?: Responsive<CSSPropertyGridArea>;
+  gridArea?: Responsive<string>;
+};
+
+const Grid = styled(Box)<GridProps>`
+  display: grid;
+  ${(props) => toPropValue("grid-gap", props.gridGap, props.theme)}
+  ${(props) => toPropValue("grid-column-gap", props.gridColumnGap)}
+    ${(props) => toPropValue("grid-row-gap", props.gridRowGap, props.theme)}
+    ${(props) => toPropValue("grid-column", props.gridColumn, props.theme)}
+    ${(props) => toPropValue("grid-row", props.gridRow, props.theme)}
+    ${(props) => toPropValue("grid-auto-flow", props.gridAutoFlow, props.theme)}
+    ${(props) =>
+    toPropValue("grid-auto-columns", props.gridAutoColumns, props.theme)}
+    ${(props) => toPropValue("grid-auto-rows", props.gridAutoRows, props.theme)}
+    ${(props) =>
+    toPropValue(
+      "grid-template-columns",
+      props.gridTemplateColumns,
+      props.theme
+    )}
+    ${(props) =>
+    toPropValue("grid-template-rows", props.gridTemplateRows, props.theme)}
+    ${(props) =>
+    toPropValue("grid-template-areas", props.gridTemplateAreas, props.theme)}
+    ${(props) => toPropValue("grid-area", props.gridArea, props.theme)}
+`;
+
+export default Grid;
