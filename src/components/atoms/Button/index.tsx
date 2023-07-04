7c860dc3e019488711661ea7a68fa834f0f453aa
teppei <teppei0830.4869@gmail.com>
2023-07-04 21:44:51 +0900

    layout/Button/index.tsx に追記しました。
    Button コンポーネントのスタイルに関して付け加えました。

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -9,6 +9,8 @@ import {
   Space,
   AppTheme,
 } from "@/util/styles";
+import { setTokenSourceMapRange } from "typescript";
+import { Viaoda_Libre } from "next/font/google";
 
 export type ButtonVariant = "primary" | "secondary" | "danger";
 
@@ -39,10 +41,12 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   paddingLeft?: Responsive<Space>;
   paddingRight?: Responsive<Space>;
   pseudoClass?: {
-    backgroundColor?: Responsive<Color>;
-  };
-  disabled?: {
-    backgroundColor?: Responsive<Color>;
+    hover?: {
+      backgroundColor?: Responsive<Color>;
+    };
+    disabled?: {
+      backgroundColor?: Responsive<Color>;
+    };
   };
   theme?: AppTheme;
 };
@@ -95,7 +99,93 @@ const Button = styled.button<ButtonProps>`
       const styles = [];
       !color &&
         styles.push(toPropValue("color", variants[variant].color, theme));
+      !backgroundColor &&
+        styles.push(
+          toPropValue(
+            "backgroundColor",
+            variants[variant].backgroundColor,
+            theme
+          )
+        );
+      !pseudoClass &&
+        styles.push(
+          `&:hover {
+            ${toPropValue(
+              "background-color",
+              variants[variant].pseudoClass.hover.backgroundColor,
+              theme
+            )}
+          }`.replaceAll("\n", "")
+        );
+      !pseudoClass &&
+        styles.push(
+          `&:disabled {
+            ${toPropValue(
+              "background-color",
+              variants[variant].pseudoClass.disabled.backgroundColor,
+              theme
+            )}
+          }`.replaceAll("\n", "")
+        );
       return styles.join("\n");
     }
   }}
+  ${(props) => toPropValue("font-size", props.fontSize, props.theme)}
+  ${(props) => toPropValue("letter-spacing", props.letterSpacing, props.theme)}
+  ${(props) => toPropValue("line-height", props.lineHeight, props.theme)}
+  ${(props) => toPropValue("color", props.color, props.theme)}
+  ${(props) =>
+    toPropValue("background-color", props.backgroundColor, props.theme)}
+  ${(props) => toPropValue("width", props.width, props.theme)}
+  ${(props) => toPropValue("height", props.height, props.theme)}
+  ${(props) => toPropValue("min-width", props.minWidth, props.theme)}
+  ${(props) => toPropValue("min-height", props.minHeight, props.theme)}
+  ${(props) => toPropValue("display", props.display, props.theme)}
+  ${(props) => toPropValue("border", props.border, props.theme)}
+  ${(props) => toPropValue("overflow", props.overflow, props.theme)}
+  ${(props) => toPropValue("margin", props.margin, props.theme)}
+  ${(props) => toPropValue("margin-top", props.marginTop, props.theme)}
+  ${(props) => toPropValue("margin-bottom", props.marginBottom, props.theme)}
+  ${(props) => toPropValue("margin-left", props.marginLeft, props.theme)}
+  ${(props) => toPropValue("margin-right", props.marginRight, props.theme)}
+  ${(props) => toPropValue("padding", props.padding, props.theme)}
+  ${(props) => toPropValue("padding-top", props.paddingTop, props.theme)}
+  ${(props) => toPropValue("padding-bottom", props.paddingBottom, props.theme)}
+  ${(props) => toPropValue("padding-left", props.paddingLeft, props.theme)}
+  ${(props) => toPropValue("padding-right", props.paddingRight, props.theme)}
+  &:hover {
+    ${(props) =>
+      toPropValue(
+        "background-color",
+        props?.pseudoClass?.hover?.backgroundColor
+      )}
+  }
+  &:disabled {
+    ${(props) =>
+      toPropValue(
+        "background-color",
+        props?.pseudoClass?.disabled?.backgroundColor
+      )}
+  }
+  cursor: pointer;
+  outline: 0;
+  text-decoration: "none";
+  opacity: ${({ disabled }) => (disabled ? "0.5" : "1")};
+  border-radius: 4px;
+  border: "none";
 `;
+
+Button.defaultProps = {
+  variant: "primary",
+  paddingLeft: 2,
+  paddingRight: 1,
+  paddingTop: 1,
+  paddingBottom: 1,
+  color: "white",
+  display: "inline-block",
+  textAlign: "center",
+  lineHeight: "inherit",
+  fontSize: "inherit",
+};
+
+export default Button;
