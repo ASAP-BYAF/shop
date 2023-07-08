1094a224b670d251349783bb2e775ee63c732af9
teppei <teppei0830.4869@gmail.com>
2023-07-08 21:36:34 +0900

    新たに src/components/atoms/Button6/index.tsx を作成しました。
    これは、与える variant を変えたときの動作を確認するためのコンポーネントです。
    Button5 の時点では variant を変更しても見た目が変化しませんでした。
    Button6 で defaultProps の設定をなくすとこれが解消され、
     primary と secondary で装飾を分けることができるようになりました。

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,6 +6,7 @@ import Button2 from "@/components/atoms/Button2/index";
 import Button3 from "@/components/atoms/Button3/index";
 import Button4 from "@/components/atoms/Button4/index";
 import Button5 from "@/components/atoms/Button5/index";
+import Button6 from "@/components/atoms/Button6/index";
 
 const Page: NextPage = () => {
   return (
@@ -18,7 +19,8 @@ const Page: NextPage = () => {
       <Button3 backgroundColor={"black"}>Button3</Button3>
       <Button4 backgroundColor={"black"}>Button4</Button4>
       <Button5 variant={"primary"}>Button5</Button5>
-      <Button5 backgroundColor={"red"}>Button5</Button5>
+      <Button6 variant={"primary"}>Button6</Button6>
+      <Button6 variant={"secondary"}>Button6</Button6>
       <button style={{ backgroundColor: "red" }}>button</button>
     </div>
   );
