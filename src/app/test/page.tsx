531b80536de578d5387637b1022d369987ba6254
teppei <teppei0830.4869@gmail.com>
2023-07-08 18:43:39 +0900

    新たに src/components/atoms/Button2/index.tsx を作成しました。
    これは、 layout.tsx の ThemeProvider の動作を確認するためのコンポーネントです。
    確かに、うまく theme を props として受け取っていることが確認できました。

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,11 +2,13 @@
 
 import { NextPage } from "next";
 import Button from "@/components/atoms/Button/index";
+import Button2 from "@/components/atoms/Button2/index";
 
 const Page: NextPage = () => {
   return (
     <div>
       <Button>Hello</Button>
+      <Button2>Hello</Button2>
       <button style={{ backgroundColor: "red" }}>Hello</button>
     </div>
   );
---
+++ src/components/atoms/Button2/index.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Button2 = styled.button`
+  background-color: ${(props) => props.theme.colors.white};
+`;
+
+export default Button2;
