96c02a7e4343f40e0d4c59b2f4ce394bab823cf4
teppei <teppei0830.4869@gmail.com>
2023-07-08 20:35:24 +0900

    新たに src/components/atoms/Button3/index.tsx を追加しました。
    これは、 toPropValue 関数の動作を確認するためのコンポーネントです。
    確かに toPropValue 関数はうまく動作していることを確認しました。

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,12 +3,14 @@
 import { NextPage } from "next";
 import Button from "@/components/atoms/Button/index";
 import Button2 from "@/components/atoms/Button2/index";
+import Button3 from "@/components/atoms/Button3/index";
 
 const Page: NextPage = () => {
   return (
     <div>
       <Button>Hello</Button>
       <Button2>Hello</Button2>
+      <Button3 backgroundColor={"black"}>Hello</Button3>
       <button style={{ backgroundColor: "red" }}>Hello</button>
     </div>
   );
