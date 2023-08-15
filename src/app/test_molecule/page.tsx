2071001de5000232fe089e106ae1a6ae93378a25
teppei <teppei0830.4869@gmail.com>
2023-08-15 17:39:30 +0900

    test_molecule のページに Dropzone を追加しました。

--- src/app/test_molecule/page.tsx
+++ src/app/test_molecule/page.tsx
@@ -2,11 +2,13 @@
 
 import { NextPage } from "next";
 import CheckBox from "@/components/molecules/CheckBox";
+import Dropzone from "@/components/molecules/Dropzone";
 
 const Page: NextPage = () => {
   return (
     <div>
       <CheckBox label="tmp" checked={false} />
+      <Dropzone />
     </div>
   );
 };
