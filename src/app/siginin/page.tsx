6bb79ddad53449765eedbe06b4f0f9d06072b241
teppei <teppei0830.4869@gmail.com>
2023-08-20 13:57:12 +0900

    siginin ページを作成しました。

---
+++ src/app/siginin/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import type { NextPage } from "next";
+import { useRouter, useSearchParams } from "next/navigation";
+import AppLogo from "@/components/atoms/AppLogo";
+import Box from "@/components/layout/Box";
+import Flex from "@/components/layout/Flex";
+import Layout from "@/components/templates/Layout";
+import SigninFormContainer from "@/containers/SigninForm";
+
+const SigninPage: NextPage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const handleSignin = async (err?: Error) => {
+    if (!err) {
+      // const redirectTo = (router.query["redirect_to"] as string) ?? "/";
+      const redirectTo = (searchParams.get("redirect_to") as string) ?? "/";
+      console.log("Redirectin", redirectTo);
+      await router.push(redirectTo);
+    }
+  };
+
+  return (
+    <Layout>
+      <Flex
+        paddingTop={2}
+        paddingBottom={2}
+        paddingLeft={{ base: 2, md: 0 }}
+        paddingRight={{ base: 2, md: 0 }}
+        justifyContent="center"
+      >
+        <Flex
+          width="400px"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box marginBottom={2}>
+            <AppLogo />
+          </Box>
+          <Box width="100%">
+            <SigninFormContainer onSignin={handleSignin} />
+          </Box>
+        </Flex>
+      </Flex>
+    </Layout>
+  );
+};
+
+export default SigninPage;
